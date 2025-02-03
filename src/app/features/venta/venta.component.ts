ed36950d635f216eec7377bdbeeacdd9e34bc4fa
deijomes <deibermndoza@gmail.com>
2025-02-03 11:57:25 -0500

    ngselct terminado

--- src/app/features/venta/venta.component.ts
+++ src/app/features/venta/venta.component.ts
@@ -1,13 +1,66 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+
+import { PoductService } from '../../services/poduct.service';
 
 @Component({
   selector: 'app-venta',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, NgSelectModule],
    schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './venta.component.html',
-  styleUrl: './venta.component.css'
+  styleUrl: './venta.component.css',
+  encapsulation: ViewEncapsulation.None 
 })
-export class VentaComponent {
+export class VentaComponent  implements OnInit{
 
-}
+  saleForm: FormGroup;
+  productos: any[] = [];
+  filteredProductos: any[] = [];
+  
+  constructor(private fb: FormBuilder, private serviceproduct: PoductService) {
+    this.saleForm = this.fb.group({
+      codigo: [''],
+      articulo: [''],
+      cantidad: ['']
+    });
+  }
+  
+  ngOnInit(): void {
+    this.producto();
+  }
+  
+  producto() {
+    this.serviceproduct.obtenerRegistros().subscribe({
+      next: (response) => {
+        this.productos = response; // Asegúrate de usar un punto y coma, no coma
+        this.filteredProductos = [...this.productos];
+        console.log(this.filteredProductos, 'este el listado');
+      },
+      error: (err) => {
+        console.error('Error al obtener productos:', err);
+      }
+    });
+  
+    // Detectar cambios en el campo "codigo" para filtrar productos
+    this.saleForm.get('name')?.valueChanges.subscribe(value => {
+      this.filtrarProductos(value);
+    });
+  }
+  
+  filtrarProductos(searchText: string) {
+    if (!searchText) {
+      this.filteredProductos = [...this.productos];
+      return;
+    }
+    this.filteredProductos = this.productos.filter(p =>
+      p.codigo?.toLowerCase().includes(searchText.toLowerCase()) // Asegúrate que "codigo" exista en cada producto
+    );
+  }
+}  
+
+ 
+
+
