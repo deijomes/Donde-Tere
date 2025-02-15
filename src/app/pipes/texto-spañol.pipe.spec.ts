80e80ad4ead85b616db2314bd55842d5fb2771b3
Kkmiloo <66806810+Kkmiloo@users.noreply.github.com>
2025-02-15 21:29:45 +0000

    Build generado el {date} desde 4835b13

--- src/app/pipes/texto-spañol.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TextoSpañolPipe } from './texto-spañol.pipe';
-
-describe('TextoSpañolPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TextoSpañolPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
