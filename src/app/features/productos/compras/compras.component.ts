75ffdbdbc7fa65383ba6e8f3010e3b3b20bfa40c
deijomes <deibermndoza@gmail.com>
2025-02-21 17:19:02 -0500

    avanceCompras

--- src/app/features/productos/compras/compras.component.ts
+++ src/app/features/productos/compras/compras.component.ts
@@ -21,7 +21,7 @@ import { NgxPaginationModule } from 'ngx-pagination';
 @Component({
   selector: 'app-compras',
   standalone: true,
-  imports: [NgSelectModule, ReactiveFormsModule, CommonModule, IdPipe, CapitalizePipe, FormsModule, SelectModule,  NgxPaginationModule],
+  imports: [NgSelectModule, ReactiveFormsModule, CommonModule, IdPipe, CapitalizePipe, FormsModule, SelectModule, NgxPaginationModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './compras.component.html',
   styleUrl: './compras.component.css',
@@ -34,9 +34,10 @@ export class ComprasComponent implements OnInit {
   productos: any = [] = []
   filteredProductos: any[] = []
   productosSeleccionados: any = []
+  proSeleccion: any = []
   facturaCompra: any = []
   registroCompras: any = []
-  selctCompra: any=[]
+  selctCompra: any = []
   selectedCountry: string | undefined;
   inventariados = true;
   noInventariados = false
@@ -46,24 +47,34 @@ export class ComprasComponent implements OnInit {
   idproduct: string = '';
   Idfactura: string = '';
   tablaProducto: boolean = false
-  mostrarHistorial : boolean =  false
+  mostrarHistorial: boolean = false
+
+  comprasNoInvenForms: FormGroup;
+  tablaProNoInventariado = false;
+  idproductNoInv: number = 0
 
   currentPage: number = 1;  // Página actual (comienza en 1)
   itemsPerPage: number = 10;  // Elementos por página (puedes cambiar este valor)
   totalItems: number = 0;  //
 
-  constructor(private serviceproduct: PoductService, private serviceCompra: ProductoCompraService, private fb: 
-    FormBuilder,private router: Router, private pdf:PdfService, private loading : LoadingService) {
+  constructor(private serviceproduct: PoductService, private serviceCompra: ProductoCompraService, private fb:
+    FormBuilder, private router: Router, private pdf: PdfService, private loading: LoadingService) {
 
     this.comprasForm = this.fb.group({
       codigo: '',
       articulo: ['', Validators.required],
       cantidad: ['', Validators.required],
       precio: ['', Validators.required]
-      
+
     });
 
-    
+    this.comprasNoInvenForms = this.fb.group({
+      nombre: ['', Validators.required],
+      cantidad: ['', Validators.required],
+      precio: ['', Validators.required]
+    })
+
+
 
     this.proveedorForm = this.fb.group({
 
@@ -76,8 +87,10 @@ export class ComprasComponent implements OnInit {
     this.loading.init();
     this.producto()
     this.registrosCompras()
-    this.inventariado(); 
-    this.cargarProductosSeleccionados()
+    this.inventariado();
+    this.cargarProductosSeleccionados();
+    this.cargarProInv()
+    
   }
   // SESION : 1 OBTENER PRODUCTO, PARA LUEGO SELCCIONARLO Y GUARDARLO EN LOCAL STORAGE...
   producto() {
@@ -92,7 +105,7 @@ export class ComprasComponent implements OnInit {
         console.error('Error al obtener productos:', err);
       }, complete: () => {
         this.loading.hide()
-       
+
       }
     });
 
@@ -124,7 +137,7 @@ export class ComprasComponent implements OnInit {
         codigo: productoEncontrado.code,
         articulo: productoEncontrado.name,
         cantidad: '',
-        precio:''
+        precio: ''
 
       });
       console.log('Producto seleccionado:', productoEncontrado);
@@ -168,7 +181,7 @@ export class ComprasComponent implements OnInit {
 
           const destino = document.getElementById('tablaDes');
           if (destino) {
-    
+
             destino.scrollIntoView({ behavior: 'smooth', block: 'start' });
           }
         }, 100);
@@ -212,9 +225,9 @@ export class ComprasComponent implements OnInit {
       showCancelButton: true,
       confirmButtonText: 'Eliminar',
       cancelButtonText: 'Cancelar',
-      
-      confirmButtonColor: '#FF6F00', 
-      cancelButtonColor: '#FF9800', 
+
+      confirmButtonColor: '#FF6F00',
+      cancelButtonColor: '#FF9800',
     }).then((result) => {
       if (result.isConfirmed) {
 
@@ -230,13 +243,13 @@ export class ComprasComponent implements OnInit {
         this.cargarProductosSeleccionados();
 
 
-         Swal.fire({
-                              title: '¡Éxito!',
-                              text: 'El producto ha sido eliminado.',
-                              icon: 'success',
-                              timer: 1000,
-                              showConfirmButton: false
-                            });
+        Swal.fire({
+          title: '¡Éxito!',
+          text: 'El producto ha sido eliminado.',
+          icon: 'success',
+          timer: 1000,
+          showConfirmButton: false
+        });
       }
     });
   }
@@ -256,7 +269,7 @@ export class ComprasComponent implements OnInit {
     const cantidad = Number((document.getElementById('cantidad') as HTMLInputElement).value);
 
     if (cantidad <= 0 || isNaN(cantidad)) {
-     
+
       Swal.fire({
         title: '¡Error!',
         text: 'Por favor ingrese una cantidad válida.',
@@ -264,7 +277,7 @@ export class ComprasComponent implements OnInit {
         confirmButtonText: 'Entendido',
         confirmButtonColor: '#FF6F00'
       });
-      
+
       return;
     }
 
@@ -275,7 +288,7 @@ export class ComprasComponent implements OnInit {
       showCancelButton: true,
       confirmButtonText: 'Actualizar',
       cancelButtonText: 'Cancelar',
-      
+
       confirmButtonColor: '#FF6F00', // Color naranja para el botón de confirmar
       cancelButtonColor: '#FF9800', // Color naranja más claro para el botón de cancelar
     }).then((result) => {
@@ -291,7 +304,7 @@ export class ComprasComponent implements OnInit {
           confirmButtonText: 'Entendido', // Cambia el texto del botón
           confirmButtonColor: '#FF6F00' // Cambia el color del botón
         });
-        
+
       }
     });
   }
@@ -313,17 +326,17 @@ export class ComprasComponent implements OnInit {
         // Actualizar la lista en el componente
         this.productosSeleccionados = productos;
 
-        
+
         Swal.fire({
           title: '¡Cantidad actualizada!',
-          
+
           icon: 'success',
           timer: 2000, // 
           timerProgressBar: true,
           showConfirmButton: false,
-         
+
         });
-        
+
       } else {
         Swal.fire('Error', 'Producto no encontrado en localStorage.', 'error');
       }
@@ -342,7 +355,7 @@ export class ComprasComponent implements OnInit {
 
 
     this.loading.show()
-    const  supplyItems  : any [] = []
+    const supplyItems: any[] = []
 
     this.serviceCompra.enviarCompra(proveedor, idenfic, purchaseItems, supplyItems).subscribe({
       next: (response) => {
@@ -351,14 +364,14 @@ export class ComprasComponent implements OnInit {
         this.Idfactura = response.id
 
         this.loading.hide();
-        
+
 
 
 
         this.serviceCompra.facturaCompra(this.Idfactura).subscribe({
           next: (facturaResponse) => {
             this.facturaCompra = facturaResponse;
-           
+
 
 
 
@@ -383,13 +396,13 @@ export class ComprasComponent implements OnInit {
           }, 1000);
         });
 
-        
+
 
         this.eliminarProductosGuardados();
         this.productosSeleccionados = [];
         this.registrosCompras()
 
-        
+
         this.tablaProducto = false
         this.proveedorForm.reset()
 
@@ -431,10 +444,10 @@ export class ComprasComponent implements OnInit {
         price: producto.price
       };
 
-      
+
 
     });
-  
+
 
 
   }
@@ -448,7 +461,7 @@ export class ComprasComponent implements OnInit {
     this.serviceCompra.registrosCompras().subscribe({
       next: (Response) => {
         this.registroCompras = Response
-        console.log('compras registros',this.registroCompras)
+        console.log('compras registros', this.registroCompras)
 
       }
     })
@@ -479,18 +492,18 @@ export class ComprasComponent implements OnInit {
 
   generatePDF() {
     setTimeout(() => {
-      this.pdf. generateFacturaPDF2(this.selctCompra);
+      this.pdf.generateFacturaPDF2(this.selctCompra);
 
     }, 500)
 
   }
   generatePDFF() {
-    this.pdf. generateFacturaPDF2(this.facturaCompra);
+    this.pdf.generateFacturaPDF2(this.facturaCompra);
   }
 
   registro() {
     this.router.navigateByUrl('productos/registrar');
-    
+
 
   }
   cancelarventa() {
@@ -502,31 +515,244 @@ export class ComprasComponent implements OnInit {
 
   }
 
-  inventariado(){
+  inventariado() {
     this.inventariados = true
     this.noInventariados = false
-    
+
   }
-  noInventariado(){
+  noInventariado() {
     this.inventariados = false
     this.noInventariados = true
-   
+
+  }
+
+  //segundo formulario- productos no inventariados
+
+
+  agregarProducto() {
+    const productoNoInven = this.comprasNoInvenForms.value;
+
+    if (!productoNoInven || !productoNoInven.nombre || !productoNoInven.cantidad || !productoNoInven.precio) {
+
+      return;
+    }
+
+    const cantidad = Number(productoNoInven.cantidad);
+    const precio = Number(productoNoInven.precio);
+
+
+    if (!this.proSeleccion) {
+      this.proSeleccion = [];
+    }
+
+    // Buscar si el producto ya está en la lista
+    const productoExistente = this.proSeleccion.find((p: any) => p.name === productoNoInven.nombre);
+
+    if (productoExistente) {
+      // Si ya existe, sumamos la cantidad
+      productoExistente.quantity += cantidad;
+    } else {
+      // Si no existe, lo agregamos a la lista
+      const productoAAgr: ventaModel = {
+        id: Date.now(), // ID único
+        name: productoNoInven.nombre,
+        code: '',
+        description: '',
+        price: precio,
+        quantity: cantidad
+      };
+
+      this.proSeleccion.push(productoAAgr);
+    }
+
+
+    localStorage.setItem('productoNoInventario', JSON.stringify(this.proSeleccion));
+
+    console.log('✅ Producto agregado:', this.proSeleccion);
+    this.tablaProNoInventariado = true;
+
+    // Desplazar a la tabla
+    setTimeout(() => {
+      const destino = document.getElementById('tablaNoInventariados');
+      if (destino) destino.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }, 100);
+
+    // Reiniciar el formulario después de agregar
+    this.comprasNoInvenForms.reset();
+  }
+
+  Total(): number {
+
+    return this.proSeleccion.reduce((sum: any, producto: any) => {
+      const totalProducto = producto.price * producto.quantity; // Total por producto
+      return sum + totalProducto; // Sumar al total general
+    }, 0);
   }
 
 
-  
+  modificar(index: number) {
+
+    this.idproductNoInv = index
+
+
+
+  }
+
+
+
+  eliminarProNoInve(index: number) {
+
+    console.log('psoicion',index)
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: 'Este producto será eliminado de la lista.',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar',
+
+      confirmButtonColor: '#FF6F00',
+      cancelButtonColor: '#FF9800',
+    }).then((result) => {
+      if (result.isConfirmed) {
+
+        let productos = JSON.parse(localStorage.getItem('productoNoInventario') || '[]');
+
+        // 2️Eliminar el producto por su índice
+        productos.splice(index, 1);
+
+
+        localStorage.setItem('productoNoInventario', JSON.stringify(productos));
+        this.proSeleccion = productos;
+
+        this.cargarProInv()
+
+
+
+
+        Swal.fire({
+          title: '¡Éxito!',
+          text: 'El producto ha sido eliminado.',
+          icon: 'success',
+          timer: 1000,
+          showConfirmButton: false
+        });
+
+        
+      }
+    });
+  }
+
+  cargarProInv() {
+    const productosGuardados = localStorage.getItem('productoNoInventario');
+    if (productosGuardados) {
+      this.proSeleccion = JSON.parse(productosGuardados); // Convertir de JSON a objeto
+      console.log('Productos cargados desde LocalStorage:', this.productosSeleccionados);
+      this.tablaProNoInventariado = true;
+    }
+
+  }
+
+
+
+  actualizarProNoInventario() {
+    const cantidad = Number((document.getElementById('cantidadModi') as HTMLInputElement).value);
+
+    if (cantidad <= 0 || isNaN(cantidad)) {
+
+      Swal.fire({
+        title: '¡Error!',
+        text: 'Por favor ingrese una cantidad válida.',
+        icon: 'info',
+        confirmButtonText: 'Entendido',
+        confirmButtonColor: '#FF6F00'
+      });
+
+      return;
+    }
+
+    // Alerta de confirmación
+    Swal.fire({
+      title: '¿Está seguro de que desea actualizar la cantidad?',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Actualizar',
+      cancelButtonText: 'Cancelar',
+
+      confirmButtonColor: '#FF6F00', // Color naranja para el botón de confirmar
+      cancelButtonColor: '#FF9800', // Color naranja más claro para el botón de cancelar
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // Si el usuario confirma, se llama a la función para modificar la cantidad
+        this.modificarproductNo(this.idproductNoInv, cantidad);
+      } else {
+        // Si el usuario cancela, no se hace nada
+        Swal.fire({
+          title: 'Cancelado',
+          text: 'La cantidad no fue modificada',
+          icon: 'info',
+          confirmButtonText: 'Entendido', // Cambia el texto del botón
+          confirmButtonColor: '#FF6F00' // Cambia el color del botón
+        });
+
+      }
+    });
+  }
+
+
+  modificarproductNo(index: number, nuevaCantidad: number) {
+    const productosGuardados = localStorage.getItem('productoNoInventario');
+
+    if (productosGuardados) {
+      let productos = JSON.parse(productosGuardados);
+
+      // Verifica si el índice es válido
+      if (index >= 0 && index < productos.length) {
+        productos[index].quantity = nuevaCantidad;
+
+        // Guardar los productos actualizados en localStorage
+        localStorage.setItem('productoNoInventario', JSON.stringify(productos));
+
+        // Actualizar la lista en el componente
+        this.proSeleccion = productos;
+
+        Swal.fire({
+          title: '¡Cantidad actualizada!',
+          icon: 'success',
+          timer: 2000,
+          timerProgressBar: true,
+          showConfirmButton: false,
+        });
+
+      } else {
+        Swal.fire('Error', 'Índice fuera de rango.', 'error');
+      }
+    }
+  }
+
+  cancelarcompraProNoIn(){
+
+    localStorage.removeItem('productoNoInventario');
+   
+    this.proSeleccion = []
+    this.tablaProNoInventariado = false
+
+
+  }
 
 
 
 
 
 
+
+
+
+
+
+  // Preparar los productos para el formato correcto
 }
 
-
-// Preparar los productos para el formato correcto
-
-  
 
 
 
