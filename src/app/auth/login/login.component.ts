bc9131df1e7e44dfbaff4e3eb39a90ac821d0c97
deijomes <deibermndoza@gmail.com>
2025-02-22 21:11:42 -0500

    ajuste1

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -20,18 +20,19 @@ export class LoginComponent implements OnInit {
 
   credenciales: UsuarioModel
   loguinForm!: FormGroup
+  spinner = false
 
-  constructor(private router: Router, private bf: FormBuilder, private servicio : CredencialesService,
-    private loadingService : LoadingService
+  constructor(private router: Router, private bf: FormBuilder, private servicio: CredencialesService,
+    private loadingService: LoadingService
   ) {
     this.credenciales = new UsuarioModel()
 
   }
 
- 
+
   ngOnInit(): void {
     this.getforms()
-    this.loadingService.init();
+
 
   }
 
@@ -52,61 +53,67 @@ export class LoginComponent implements OnInit {
   loguearse(): void {
     // Si el formulario es inválido, no sigue con el registro
     if (!this.loguinForm.valid) {
-     
+
       return; // Sale de la función si el formulario no es válido
     }
 
     const usuario = this.loguinForm.value;
-   
+
+    this.spinner = true
+
     if (usuario.rememberMe) {
       localStorage.setItem('email', usuario.email);
-     
+
     } else {
       localStorage.removeItem('email');
-      
+
     }
 
-     // Verificamos si la respuesta contiene el email antes de guardarlo
-    
-    this.loadingService.show(); 
+    // Verificamos si la respuesta contiene el email antes de guardarlo
+
+
 
     this.servicio.login(usuario).subscribe(
       (resp: any) => {
 
         if (resp?.email) {
           localStorage.setItem('email', resp.email);
-          
+
         } else {
-          
+
         }
-    
-       
+
+
         if (resp && resp.token) {
-          
-    
-          this.loadingService.hide(); 
-    
-          
+
+          this.spinner = false
+
+
+
+
+
           this.router.navigateByUrl('/home');
         } else {
-         
-          this.loadingService.hide(); 
+
+
         }
       },
       (error: any) => {
         console.error('Error al iniciar sesión:', error);
-        this.loadingService.hide(); 
-       
-         Swal.fire({
-                  title: 'error',
-                  text: 'No se pudo iniciar sesión. Verifica tus credenciales e intenta nuevamente',
-                  icon: 'info',
-                  confirmButtonText: 'Entendido', // Cambia el texto del botón
-                  confirmButtonColor: '#FF6F00' // Cambia el color del botón
-                });
+
+        this.spinner = false
+
+
+        Swal.fire({
+          title: 'error',
+          text: 'No se pudo iniciar sesión. Verifica tus credenciales e intenta nuevamente',
+          icon: 'info',
+          confirmButtonText: 'Entendido', // Cambia el texto del botón
+          confirmButtonColor: '#FF6F00' // Cambia el color del botón
+        });
       }
     );
-    
+
   }
 
 
