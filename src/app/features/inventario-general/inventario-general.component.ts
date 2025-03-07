035e21712b8b5d9a6767c7519674113c72d84ac7
deijomes <deibermndoza@gmail.com>
2025-03-07 17:40:16 -0500

    InventarioGeneral

--- src/app/features/inventario-general/inventario-general.component.ts
+++ src/app/features/inventario-general/inventario-general.component.ts
@@ -4,11 +4,13 @@ import { BuscadorService } from '../../services/buscador.service';
 import { registroModel } from '../../models/registroModel';
 import { PdfService } from '../../services/pdf.service';
 import { LoadingService } from '../../services/loading.service';
+import { PoductService } from '../../services/poduct.service';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-inventario-general',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgxPaginationModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './inventario-general.component.html',
   styleUrl: './inventario-general.component.css'
@@ -16,12 +18,20 @@ import { LoadingService } from '../../services/loading.service';
 export class InventarioGeneralComponent implements OnInit {
 
 
-  productos:registroModel []= []; 
+  productos:any []= []; 
   productosFiltrados: registroModel[] = [];
   BusquedaActiva: boolean = false; 
+
+  currentPage: number = 1;  // Página actual (comienza en 1)
+  itemsPerPage: number = 20;  // Elementos por página (puedes cambiar este valor)
+  totalItems: number = 0;  // Total de productos que vamos a paginar
+  modalInstance: any;
+
   
   
-  constructor( private servicio : BuscadorService, private pdf: PdfService, private loading:LoadingService){
+  constructor( private servicio : BuscadorService, private pdf: PdfService, private loading:LoadingService,
+    private http: PoductService
+  ){
 
     
     
@@ -29,10 +39,32 @@ export class InventarioGeneralComponent implements OnInit {
   ngOnInit(): void {
 
     this.loading.init();
-    this.loading.show();
-    this.loading.hide()
+    this.registros()
+    
   
   }
+
+  registros(): void {
+    
+  
+    this.loading.show()
+    this.http.obtenerRegistros().subscribe({
+      next: (response) => {
+        this.productos = response.data;  
+       
+        
+      },
+      error: (error) => {
+        console.error('Error al obtener productos:', error);
+        this.loading.hide()
+      },
+      complete: () => {
+        this.loading.hide()
+       
+      }
+    });
+  }
+  
 
     
    
